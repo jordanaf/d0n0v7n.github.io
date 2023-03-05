573b992430afd8d7427a38c115e1841f32ab28ae
Donovan <donovan.fidele@gmail.com>
2023-03-05 22:13:00 +0400

    buildv1

---
+++ _next/static/xJ0AqmG2K4jGl61OZkTzO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/26-4fddab6bb4fc4953.js","static/chunks/pages/index-3d40d9c67e54b2cf.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
